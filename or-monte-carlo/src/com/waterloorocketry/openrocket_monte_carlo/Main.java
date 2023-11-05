c15bbde37e4c8fba74822a8bd2318ebe1d34cc05
Patrick Gu <patrick@patrickgu.ca>
2023-11-04 20:55:19 -0400

    Add openrocket files and simulate them

--- or-monte-carlo/src/com/waterloorocketry/openrocket_monte_carlo/Main.java
+++ or-monte-carlo/src/com/waterloorocketry/openrocket_monte_carlo/Main.java
@@ -7,11 +7,14 @@ import net.sf.openrocket.database.Databases;
 import net.sf.openrocket.document.OpenRocketDocument;
 import net.sf.openrocket.document.Simulation;
 import net.sf.openrocket.file.GeneralRocketLoader;
+import net.sf.openrocket.gui.util.SwingPreferences;
 import net.sf.openrocket.plugin.PluginModule;
 import net.sf.openrocket.startup.Application;
 import net.sf.openrocket.startup.GuiModule;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     /**
@@ -20,23 +23,30 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
         initializeOpenRocket();
+        List<File> thrustCurveFiles = new ArrayList<>(getOpenRocketPreferences().getUserThrustCurveFiles());
+        thrustCurveFiles.add(new File("./rockets/Kismet_v4_C2-2.rse"));
+        getOpenRocketPreferences().setUserThrustCurveFiles(thrustCurveFiles);
 
-        File file = new File("./orhelper/examples/simple.ork");
+        File file = new File("./rockets/c31a.ork");
         GeneralRocketLoader loader = new GeneralRocketLoader(file);
 
         OpenRocketDocument doc = loader.load();
+        for (int i = 1; i <= 100; i++) {
+            Simulation sim = new Simulation(doc, doc.getRocket());
+            sim.simulate();
+            double maxAltitude = sim.getSimulatedData().getMaxAltitude();
+            double maxMach = sim.getSimulatedData().getMaxMachNumber();
 
-        System.out.println(doc.getSimulationCount());
-
-        Simulation sim = doc.getSimulation(0);
-
-        sim.simulate();
-        System.out.println(sim.getSimulatedData().getMaxAcceleration());
+            System.out.println("Simulation " + i + ": apogee " + maxAltitude + " mach " + maxMach);
+        }
     }
 
     /**
      * Inject required dependencies for OpenRocket, allowing us to run simulations
      * programmatically.
+     *
+     * This runs the same code as for starting up a GUI version of OpenRocket, making it easier to make manual
+     * simulation runs automatic.
      */
     private static void initializeOpenRocket() {
         GuiModule guiModule = new GuiModule();
@@ -45,5 +55,13 @@ public class Main {
         Application.setInjector(injector);
         guiModule.startLoader();
         Databases.fakeMethod();
+    }
+
+    /**
+     * Get the preferences of OpenRocket Swing
+     * @return Preferences object
+     */
+    private static SwingPreferences getOpenRocketPreferences() {
+        return (SwingPreferences) Application.getPreferences();
     }
 }
